44ebe925e10780e740ed8a9dd87701ed1fb66f6a
Shreya Basker <shreya.basker@gmail.com>
2026-06-10 05:16:23 +0000

    added task1.py

---
+++ task1.py
@@ -0,0 +1,11 @@
+from collections import Counter
+import re # regular expressions
+def word_counter(string1):
+    words=re.findall(r'\b[a-zA-Z]+\b',string1.lower())
+    counts=Counter(words)
+    return counts.most_common(5)
+
+def main():
+    text="""Hello world! Hello everyone.
+    This is the python programming world. Pyrhon. Python"""
+    print(word_counter(text))
