c6cfcb737728c3c520038ebf108aa2b86c07ab9a
Shreya Basker <shreya.basker@gmail.com>
2026-06-10 08:56:48 +0000

    modified main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from fastapi import FastAPI
-app=FastAPI(
-    title = "Intern Project API",
-    description = "My first API application",
-    version = "1.0.0"
-)
+from pydantic import BaseModel
+user_db={
+    1: {
+        "id": 1,
+        "name": "Shreya",
+        "email": "shreya@gmail.com",
+        "active": True
+    },
+    2: {
+        "id": 2,
+        "name": "Basker",
+        "email": "basker@gmail.com",
+        "active": True
+    },
+    3: {
+        "id": 3,
+        "name": "Pankajam",
+        "email": "panki@gmail.com",
+        "active": True
+    }
+}
+Class User(BaseModel):
 
-@app.get("/")
-def root():
-    return {"message":"hello intern"}
-
-@app.get("/health")
-def health():
-    return {"status":"ok"}
+app=FastAPI()
